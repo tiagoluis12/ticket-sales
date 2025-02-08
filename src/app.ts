bc3b9ee328f692aef2ece43bfc7993b38a643299
“tiagoluis12” <tiago7164@hotmail.com>
2025-02-08 23:14:14 +0000

    API

--- src/app.ts
+++ src/app.ts
@@ -22,10 +22,19 @@ app.post('/customers', (req, res) => {
   const { name, email, password, address, phone } = req.body;
 })
 
-app.post('/events', (req, res) => {
+app.post('/partners/events', (req, res) => {
   const { name, description, date, location } = req.body;
 })
 
+app.get('/partners/events', (req, res) => {
+
+})
+
+app.get('/partners/events/:eventId', (req, res) => {
+  const {eventId} = req.params;
+  console.log (eventId);
+  res.send();
+})
 app.get('/events', (req, res) => {
 
 })
